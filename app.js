743cf76ef0573d756c071540577b08e67f2e5d00
GirugaCode <ryan.dphu.nguyen@gmail.com>
2018-10-11 17:22:46 -0700

    Displays an example of To-Do list

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
 
+//MOCK
+let toDoList = [
+  { Task: "Buy Eggs", Details: "Get brown" },
+  { Task: "Finish BEW Contractor project", Details: "Due Tomorrow" }
+]
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+
+// INDEX
 app.get('/',(req, res) => {
-  res.render('home', { msg: 'Lets make a To-Do list!'});
+  res.render('todo-index', { toDoList: toDoList });
 })
+
 
 app.listen(4000, () => {
   console.log('App listening on port 4000!')
