a6ebe960c5e1eb27d58aad676afc732ca314c08a
GirugaCode <ryan.dphu.nguyen@gmail.com>
2018-10-11 20:40:35 -0700

    Added MongoDB and List model

--- app.js
+++ app.js
@@ -4,15 +4,36 @@ const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
 
-//MOCK
-let toDoList = [
-  { Task: "Buy Eggs", Details: "Get brown" },
-  { Task: "Finish BEW Contractor project", Details: "Due Tomorrow" }
-]
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/Todo-lists');
+
+const Lists = mongoose.model('Lists', {
+  Task: String,
+  Details: String
+});
+
+// //MOCK
+// let toDoList = [
+//   { Task: "Buy Eggs", Details: "Get brown" },
+//   { Task: "Finish BEW Contractor project", Details: "Due Tomorrow" },
+//   { Task: "Buy clipper cards for guest", Details: "Pick up at walgreens"}
+// ]
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+
+
+// INDEX
+app.get('/', (req, res) => {
+  Lists.find()
+    .then(toDoList => {
+      res.render('todo-index', { toDoList: toDoList });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
 
 // INDEX
 app.get('/',(req, res) => {
